963556e77483bdcfe1f4d271d62a8df3803fa26c
octocat <octo@cat.com>
2012-10-30 08:51:42 -0600

    adding created employee table script

---
+++ config/out/populate-employees.sql
@@ -0,0 +1,400 @@
+INSERT INTO employee
+    VALUES (
+      1,
+      YTNNFLCF,
+      T,
+      2
+      );
+
+INSERT INTO employee
+    VALUES (
+      2,
+      DKCNUHIV,
+      T,
+      3
+      );
+
+INSERT INTO employee
+    VALUES (
+      3,
+      GKENUDCJ,
+      T,
+      4
+      );
+
+INSERT INTO employee
+    VALUES (
+      4,
+      RDTSTGJA,
+      F,
+      5
+      );
+
+INSERT INTO employee
+    VALUES (
+      5,
+      BSFIRSIS,
+      F,
+      6
+      );
+
+INSERT INTO employee
+    VALUES (
+      6,
+      CVIULJCS,
+      T,
+      7
+      );
+
+INSERT INTO employee
+    VALUES (
+      7,
+      XBTENNXG,
+      T,
+      8
+      );
+
+INSERT INTO employee
+    VALUES (
+      8,
+      SXWGMXCT,
+      T,
+      9
+      );
+
+INSERT INTO employee
+    VALUES (
+      9,
+      HYMATIPN,
+      T,
+      10
+      );
+
+INSERT INTO employee
+    VALUES (
+      10,
+      EEXQMSSG,
+      F,
+      11
+      );
+
+INSERT INTO employee
+    VALUES (
+      11,
+      EINGBPSW,
+      T,
+      12
+      );
+
+INSERT INTO employee
+    VALUES (
+      12,
+      SCTXABAU,
+      T,
+      13
+      );
+
+INSERT INTO employee
+    VALUES (
+      13,
+      LJAAVBQF,
+      F,
+      14
+      );
+
+INSERT INTO employee
+    VALUES (
+      14,
+      HXAABSBZ,
+      T,
+      15
+      );
+
+INSERT INTO employee
+    VALUES (
+      15,
+      ZKNCTQGY,
+      F,
+      16
+      );
+
+INSERT INTO employee
+    VALUES (
+      16,
+      HPLQGKEK,
+      T,
+      17
+      );
+
+INSERT INTO employee
+    VALUES (
+      17,
+      QAWEGLEG,
+      T,
+      18
+      );
+
+INSERT INTO employee
+    VALUES (
+      18,
+      KDZAFHYO,
+      F,
+      19
+      );
+
+INSERT INTO employee
+    VALUES (
+      19,
+      GHVCQJGX,
+      T,
+      20
+      );
+
+INSERT INTO employee
+    VALUES (
+      20,
+      DCQFNNZJ,
+      T,
+      21
+      );
+
+INSERT INTO employee
+    VALUES (
+      21,
+      XKOEQHGJ,
+      T,
+      22
+      );
+
+INSERT INTO employee
+    VALUES (
+      22,
+      NHBAEJRJ,
+      F,
+      23
+      );
+
+INSERT INTO employee
+    VALUES (
+      23,
+      JKYEEABZ,
+      F,
+      24
+      );
+
+INSERT INTO employee
+    VALUES (
+      24,
+      AHSCXBHL,
+      F,
+      25
+      );
+
+INSERT INTO employee
+    VALUES (
+      25,
+      FEMJCQQN,
+      F,
+      26
+      );
+
+INSERT INTO employee
+    VALUES (
+      26,
+      VGDKXGRM,
+      F,
+      27
+      );
+
+INSERT INTO employee
+    VALUES (
+      27,
+      JLYALDZS,
+      T,
+      28
+      );
+
+INSERT INTO employee
+    VALUES (
+      28,
+      PVOCETMB,
+      T,
+      29
+      );
+
+INSERT INTO employee
+    VALUES (
+      29,
+      JSICYHFX,
+      T,
+      30
+      );
+
+INSERT INTO employee
+    VALUES (
+      30,
+      KNAVWXUY,
+      F,
+      31
+      );
+
+INSERT INTO employee
+    VALUES (
+      31,
+      ULTYBOJL,
+      T,
+      32
+      );
+
+INSERT INTO employee
+    VALUES (
+      32,
+      MWEUGLBT,
+      F,
+      33
+      );
+
+INSERT INTO employee
+    VALUES (
+      33,
+      KBAHQYQS,
+      T,
+      34
+      );
+
+INSERT INTO employee
+    VALUES (
+      34,
+      FTVTZZMD,
+      T,
+      35
+      );
+
+INSERT INTO employee
+    VALUES (
+      35,
+      JFBEWKEZ,
+      T,
+      36
+      );
+
+INSERT INTO employee
+    VALUES (
+      36,
+      DPKDXSNG,
+      T,
+      37
+      );
+
+INSERT INTO employee
+    VALUES (
+      37,
+      ZCKGUMWU,
+      F,
+      38
+      );
+
+INSERT INTO employee
+    VALUES (
+      38,
+      QJGEOQJP,
+      T,
+      39
+      );
+
+INSERT INTO employee
+    VALUES (
+      39,
+      UTEJOLCF,
+      T,
+      40
+      );
+
+INSERT INTO employee
+    VALUES (
+      40,
+      XWCOIGFM,
+      F,
+      41
+      );
+
+INSERT INTO employee
+    VALUES (
+      41,
+      KLUVQIIF,
+      T,
+      42
+      );
+
+INSERT INTO employee
+    VALUES (
+      42,
+      NQGVLBHG,
+      T,
+      43
+      );
+
+INSERT INTO employee
+    VALUES (
+      43,
+      LGPDFLUY,
+      T,
+      44
+      );
+
+INSERT INTO employee
+    VALUES (
+      44,
+      JYMXDCOU,
+      T,
+      45
+      );
+
+INSERT INTO employee
+    VALUES (
+      45,
+      LYGXDQIB,
+      F,
+      46
+      );
+
+INSERT INTO employee
+    VALUES (
+      46,
+      ZYZJXISL,
+      F,
+      47
+      );
+
+INSERT INTO employee
+    VALUES (
+      47,
+      HPCYVQSU,
+      T,
+      48
+      );
+
+INSERT INTO employee
+    VALUES (
+      48,
+      WQRSKETQ,
+      F,
+      49
+      );
+
+INSERT INTO employee
+    VALUES (
+      49,
+      PEVENBUN,
+      F,
+      50
+      );
+
+INSERT INTO employee
+    VALUES (
+      50,
+      DDXTIIJV,
+      T,
+
+      );
+
