ce7a1c3b547864c7c54501c0431cea8edefe1dbd
Devin Brown <devin.g.brown@gmail.com>
2012-11-25 10:50:23 -0700

    adding required JDBC connection driver, jar, cleaning up connection functionality, connects to DB, #2

--- src/music/store/Main.java
+++ src/music/store/Main.java
@@ -1,5 +1,28 @@
 package music.store;
 
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
+    final static String MUSIC_STORE_URL = "jdbc:oracle:thin:@//Cncsidb01.msudenver.edu:1521/db01";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        java.sql.Connection conn = DriverManager.getConnection(MUSIC_STORE_URL, "<username>", "<password>");
+
+        conn.setAutoCommit(false);
+
+        Statement stmt = conn.createStatement();
+        ResultSet connectionInfo = stmt.executeQuery("SELECT BANNER FROM SYS.V_$VERSION");
+
+        while (connectionInfo.next()) {
+            System.out.println(connectionInfo.getString(1));
+        }
+
+        stmt.close();
+
+    }
 }
