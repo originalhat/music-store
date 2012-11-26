c737d3f28d6246f2cab9d698a23c93530b3aa3af
Devin Brown <devin.g.brown@gmail.com>
2012-11-25 17:54:37 -0700

    adding a 2-dimmm datastructure for containing queried music table data, #2

--- src/music/store/Main.java
+++ src/music/store/Main.java
@@ -2,9 +2,9 @@ package music.store;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -13,14 +13,20 @@ public class Main {
     final static String PASSWORD = "W3lc0m3";
 
     public static void main(String[] args) {
+
+        ArrayList<String[]> musicItems = null;
+
         try {
-            selectAllMusic();
+            musicItems = selectMusic("WHERE MUSIC_TITLE='ESGFILXD'");
         } catch (SQLException e) {
             System.out.println("ERROR: " + e);
         }
+
+        // testing / example
+        // System.out.println(musicItems.get(0)[0]);
     }
 
-    private static void selectAllMusic() throws SQLException {
+    private static ArrayList<String[]> selectMusic(String condition) throws SQLException {
 
         java.sql.Connection connection = DriverManager.getConnection(MUSIC_STORE_URL, USERNAME, PASSWORD);
 
@@ -28,43 +34,33 @@ public class Main {
         connection.setAutoCommit(false);
 
         Statement statement = connection.createStatement();
-        ResultSet query = statement.executeQuery("SELECT * FROM music");
+        ResultSet query = statement.executeQuery("SELECT * FROM music " + condition);
 
-        ResultSetMetaData queryMetaData = query.getMetaData();
-
-        int numberOfColumns = queryMetaData.getColumnCount();
-
-        System.out.println("TABLE COLUMNS:");
-        for (int i = 1; i <= numberOfColumns; i++) {
-            System.out.println(">>" + queryMetaData.getColumnName(i));
-        }
+        /*
+         * adding a 2 dimensional array(list); unlimited rows, fixed column (9)
+         * width; holds the extracted contents from the 'music' table
+         */
+        ArrayList<String[]> musicItems = new ArrayList<String[]>();
+        String[] rowAttr = new String[9];
 
         while (query.next()) {
-            String music_TITLE = query.getString("MUSIC_TITLE");
-            String music_ISBN = query.getString("MUSIC_ISBN");
-            String music_type = query.getString("MUSIC_TYPE");
-            String producer = query.getString("PRODUCER");
-            String genre = query.getString("GENRE");
-            String year = query.getString("YEAR");
-            String vendor_ID = query.getString("VENDOR_ID");
-            String price = query.getString("PRICE");
-            String quanity = query.getString("QUANTITY");
+            rowAttr[0] = query.getString("MUSIC_TITLE");
+            rowAttr[1] = query.getString("MUSIC_ISBN");
+            rowAttr[2] = query.getString("MUSIC_TYPE");
+            rowAttr[3] = query.getString("PRODUCER");
+            rowAttr[4] = query.getString("GENRE");
+            rowAttr[5] = query.getString("YEAR");
+            rowAttr[6] = query.getString("VENDOR_ID");
+            rowAttr[7] = query.getString("PRICE");
+            rowAttr[8] = query.getString("QUANTITY");
 
-            System.out.print("| " + music_TITLE + " | ");
-            System.out.print(music_ISBN + " | ");
-            System.out.print(music_type + " | ");
-            System.out.print(producer + " | ");
-            System.out.print(genre + " | ");
-            System.out.print(year + " | ");
-            System.out.print(vendor_ID + " | ");
-            System.out.print(price + " | ");
-            System.out.print(quanity + " |\n");
-
-            // break;
+            musicItems.add(rowAttr);
 
         }
 
         statement.close();
+
+        return musicItems;
 
     }
 }
