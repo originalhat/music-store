40bb1b5137e068869415087cfe509d735efc3c5b
Devin Brown <devin.g.brown@gmail.com>
2012-11-27 11:17:22 -0700

    extracting query functionality to independent class for easy library / API-ish functionality

--- src/music/store/Main.java
+++ src/music/store/Main.java
@@ -1,23 +1,17 @@
 package music.store;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Main {
-
-    final private static String MUSIC_STORE_URL = "jdbc:oracle:thin:@//Cncsidb01.msudenver.edu:1521/db01";
-    final private static String USERNAME = "bgill9";
-    final private static String PASSWORD = "W3lc0m3";
 
     public static void main(String[] args) {
 
         try {
-            displayQueryResults(queryDatabase("SELECT price FROM music WHERE price < 10"));
+
+            MusicStoreQuery newQuery = new MusicStoreQuery();
+
+            newQuery.displayQueryResults(newQuery.queryDatabase("SELECT price FROM music WHERE price < 10"));
+
         } catch (SQLException e) {
             System.out.println("SQL ERROR: " + e);
         } catch (IndexOutOfBoundsException e) {
@@ -25,55 +19,4 @@ public class Main {
         }
     }
 
-    private static void displayQueryResults(ArrayList<HashMap<String, String>> musicTable) {
-
-        System.out.println(musicTable.size());
-
-        for (int rowNumber = 0; rowNumber < musicTable.size(); rowNumber++) {
-            System.out.println(musicTable.get(rowNumber));
-        }
-    }
-
-    private static ArrayList<HashMap<String, String>> queryDatabase(String query) throws SQLException {
-
-        Statement statement = getConnection();
-        ResultSet queryResult = statement.executeQuery(query);
-        ResultSetMetaData queryMetaData = getQueryMetaData(queryResult);
-
-        ArrayList<HashMap<String, String>> musicItems = new ArrayList<HashMap<String, String>>();
-
-        // row level
-        while (queryResult.next()) {
-
-            HashMap<String, String> musicTableRow = new HashMap<String, String>();
-
-            // column level
-            for (int i = 1; i <= queryMetaData.getColumnCount(); i++) {
-
-                String columnName = queryMetaData.getColumnName(i);
-
-                if (columnName != null) {
-                    musicTableRow.put(columnName, queryResult.getString(columnName));
-                }
-            }
-
-            musicItems.add(musicTableRow);
-
-        }
-
-        statement.close();
-
-        return musicItems;
-
-    }
-
-    private static ResultSetMetaData getQueryMetaData(ResultSet queryResult) throws SQLException {
-        return queryResult.getMetaData();
-    }
-
-    private static Statement getConnection() throws SQLException {
-        java.sql.Connection connection = DriverManager.getConnection(MUSIC_STORE_URL, USERNAME, PASSWORD);
-        Statement statement = connection.createStatement();
-        return statement;
-    }
 }
---
+++ src/music/store/MusicStoreQuery.java
@@ -0,0 +1,65 @@
+package music.store;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MusicStoreQuery {
+
+    final private String MUSIC_STORE_URL = "jdbc:oracle:thin:@//Cncsidb01.msudenver.edu:1521/db01";
+    final private String USERNAME = "bgill9";
+    final private String PASSWORD = "W3lc0m3";
+
+    public ArrayList<HashMap<String, String>> queryDatabase(String query) throws SQLException {
+
+        Statement statement = getConnection();
+        ResultSet queryResult = statement.executeQuery(query);
+        ResultSetMetaData queryMetaData = getQueryMetaData(queryResult);
+
+        ArrayList<HashMap<String, String>> musicItems = new ArrayList<HashMap<String, String>>();
+
+        // row level
+        while (queryResult.next()) {
+
+            HashMap<String, String> musicTableRow = new HashMap<String, String>();
+
+            // column level
+            for (int i = 1; i <= queryMetaData.getColumnCount(); i++) {
+
+                String columnName = queryMetaData.getColumnName(i);
+
+                if (columnName != null) {
+                    musicTableRow.put(columnName, queryResult.getString(columnName));
+                }
+            }
+
+            musicItems.add(musicTableRow);
+
+        }
+
+        statement.close();
+
+        return musicItems;
+
+    }
+
+    public void displayQueryResults(ArrayList<HashMap<String, String>> musicTable) {
+        for (int rowNumber = 0; rowNumber < musicTable.size(); rowNumber++) {
+            System.out.println(musicTable.get(rowNumber));
+        }
+    }
+
+    private ResultSetMetaData getQueryMetaData(ResultSet queryResult) throws SQLException {
+        return queryResult.getMetaData();
+    }
+
+    private Statement getConnection() throws SQLException {
+        java.sql.Connection connection = DriverManager.getConnection(MUSIC_STORE_URL, USERNAME, PASSWORD);
+        Statement statement = connection.createStatement();
+        return statement;
+    }
+}
